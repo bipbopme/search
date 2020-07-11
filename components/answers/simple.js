c7f2506a9cef9b0eef7bbda01dc997bb4639d123
Nick Chapman <nchapman@gmail.com>
2020-07-10 19:12:20 -0700

    Added calculator enhancer.

--- components/answers/simple.js
+++ components/answers/simple.js
@@ -4,9 +4,16 @@ export default function SimpleAnswer({ title, subtitle, source, sourceUrl, sourc
       <h3 className="title">{title}</h3>
       <div className="subtitle">{subtitle}</div>
       <div className="source">
-        <a href={sourceUrl}>
-          <img src={sourceIconUrl} width="16" height="16" /> More from {source}
-        </a>
+        {sourceUrl && (
+          <a href={sourceUrl}>
+            <img src={sourceIconUrl} width="16" height="16" /> More from {source}
+          </a>
+        )}
+        {!sourceUrl && source && (
+          <>
+            <img src={sourceIconUrl} width="16" height="16" /> {source}
+          </>
+        )}
       </div>
     </div>
   );
