c4efc7eb50a2007273fa48e1601e279e7e075878
Nick Chapman <nchapman@gmail.com>
2020-07-15 16:51:53 -0700

    Made Wikipedia card expandable.

--- components/supplements/wikipedia.js
+++ components/supplements/wikipedia.js
@@ -1,3 +1,13 @@
+import { useState } from 'react';
+
+function truncate(text, length = 350) {
+  if (text.length > length) {
+    return text.substring(0, length).replace(/[\.\s]+$/, '') + '…';
+  } else {
+    return text;
+  }
+}
+
 export default function Wikpedia({
   title,
   url,
@@ -8,20 +18,39 @@ export default function Wikpedia({
   source,
   sourceIconUrl
 }) {
+  const [collapsed, setCollapsed] = useState(true);
+
+  function toggleCollapsed() {
+    setCollapsed(!collapsed);
+  }
+
   return (
     <div className="supplement wikipedia">
-      <h3 className="title">{title}</h3>
-      <div className="summary">
-        {images[0] && <img src={images[0]} />}
-        {summary}
+      <div className="content">
+        <h3 className="title">{title}</h3>
+        <div className="summary">
+          {images[0] && <img src={images[0]} />}
+          {collapsed ? truncate(summary) : summary}
+        </div>
+        <div className="source">
+          <a href={url}>
+            <img src={sourceIconUrl} width="16" height="16" /> More from {source}
+          </a>
+        </div>
+        {infoBoxPreview && (
+          <div
+            className="infoBox"
+            dangerouslySetInnerHTML={{ __html: collapsed ? infoBoxPreview : infoBox }}
+          ></div>
+        )}
       </div>
-      <div className="source">
-        <a href={url}>
-          <img src={sourceIconUrl} width="16" height="16" /> More from {source}
-        </a>
-      </div>
-      {infoBoxPreview && (
-        <div className="infoBox" dangerouslySetInnerHTML={{ __html: infoBoxPreview }}></div>
+      {collapsed && (
+        <footer className="more">
+          <a onClick={toggleCollapsed}>
+            <img className="icon" src="/images/icons/line-height.svg" height="16" width="16" /> Show
+            more
+          </a>
+        </footer>
       )}
     </div>
   );
