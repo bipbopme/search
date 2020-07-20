3a7b3a6e752eab968a7fe21da9c9f90f4bd9b83d
Nick Chapman <nchapman@gmail.com>
2020-07-20 13:35:17 -0700

    Logging exceptions to Sentry.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+if (process.env.SENTRY_DSN) {
+  require('@sentry/node').init({
+    dsn: process.env.SENTRY_DSN
+  });
+}
+
 module.exports = {
   env: {
     MAPBOX_TOKEN: process.env.MAPBOX_TOKEN
