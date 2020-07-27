e82dede67a6ff89fb925da76fe00f31f57632839
Nick Chapman <nchapman@gmail.com>
2020-07-27 16:52:33 -0700

    Standardized image searches.

--- components/results/image/list.js
+++ components/results/image/list.js
@@ -1,10 +1,10 @@
 import InlineImageGrid from '../../shared/inlineImageGrid';
 
-export default function ImageList({ query, response }) {
+export default function ImageList({ query, results }) {
   return (
     <div className="imageList">
       <h3 className="sectionTitle">Images</h3>
-      <InlineImageGrid images={response.results} rows={2} query={query} rowHeight={150} />
+      <InlineImageGrid images={results} rows={2} query={query} rowHeight={150} />
     </div>
   );
 }
