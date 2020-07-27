19f996888d0ec4baa21daaa33ad722c64ef52746
Nick Chapman <nchapman@gmail.com>
2020-07-27 15:56:46 -0700

    Improved inline image result placement.

--- components/results/image/list.js
+++ components/results/image/list.js
@@ -4,7 +4,7 @@ export default function ImageList({ query, response }) {
   return (
     <div className="imageList">
       <h3 className="sectionTitle">Images</h3>
-      <InlineImageGrid images={response.results} rows={3} query={query} />
+      <InlineImageGrid images={response.results} rows={2} query={query} rowHeight={150} />
     </div>
   );
 }
