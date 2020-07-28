81e93244622649be1f47926a58d18808e8b3be98
Nick Chapman <nchapman@gmail.com>
2020-07-28 09:42:31 -0700

    Added custom title for GIF results.

--- components/results/image/list.js
+++ components/results/image/list.js
@@ -1,9 +1,9 @@
 import InlineImageGrid from '../../shared/inlineImageGrid';
 
-export default function ImageList({ query, results }) {
+export default function ImageList({ query, results, title }) {
   return (
     <div className="imageList">
-      <h3 className="sectionTitle">Images</h3>
+      <h3 className="sectionTitle">{title || 'Images'}</h3>
       <InlineImageGrid images={results} rows={3} query={query} rowHeight={120} />
     </div>
   );
