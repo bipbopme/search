d91cd7bbbcfe192674a64e54bc39ce160d09feff
Nick Chapman <nchapman@gmail.com>
2020-07-31 11:02:18 -0700

    Fancier navigational search results.

--- components/results/webPage/item.js
+++ components/results/webPage/item.js
@@ -1,11 +1,43 @@
-export default function webPage({ name, url, displayUrl, snippet }) {
+import { useState } from 'react';
+
+export default function webPage({ name, url, displayUrl, snippet, deepLinks, isNavigational }) {
+  const { host } = new URL(url);
+  const [faviconSrc, setFaviconSrc] = useState(`https://${host}/favicon.ico`);
+
+  function handleImageError() {
+    setFaviconSrc('/images/icons/checkbox-circle-line.svg');
+  }
+
   return (
-    <div className="webPage">
+    <div className={`webPage ${isNavigational ? 'isNavigational' : 'notNavigational'}`}>
       <h4 className="name">
-        <a href={url}>{name}</a>
+        <a href={url}>
+          {isNavigational && (
+            <img
+              className="logo"
+              src={faviconSrc}
+              height="16"
+              width="16"
+              onError={handleImageError}
+            />
+          )}
+          {name}
+        </a>
       </h4>
       <div className="displayUrl">{displayUrl}</div>
       <div className="snippet">{snippet}</div>
+      {isNavigational && deepLinks && (
+        <div className="deepLinks">
+          {deepLinks.map(deepLink => (
+            <div className="deepLink">
+              <div className="name">
+                <a href={deepLink.url}>{deepLink.name}</a>
+              </div>
+              <div className="snippet">{deepLink.snippet}</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
